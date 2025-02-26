96ae7a24911017217789348ee141c568fc6431ab
Pedro Correia <peeucorreiabr@gmail.com>
2025-02-25 21:51:22 -0300

    Adição das views no consulta.sql

--- Consultas.sql
+++ Consultas.sql
@@ -6,11 +6,12 @@
  */
 
 -- Consulta 01: visualização dos preços ao longo de um determinado período de tempo
+CREATE MATERIALIZED VIEW mv_preco_medio_tempo AS
 SELECT 
-	dd.ano,
-	dd.mes,
-	dd.mes_por_extenso,
-	AVG(fc.preco) AS preco_medio
+dd.ano,
+dd.mes,
+dd.mes_por_extenso,
+AVG(fc.preco) AS preco_medio
 FROM fato_cotacao fc
 JOIN dim_data dd ON fc.id_data = dd.id_data
 WHERE fc.id_produto = <ID_PRODUTO> -- Alterar para o produto desejado no filtro
@@ -19,6 +20,7 @@ ORDER BY preco_medio
 
 
 -- Consulta 02: preço médio de um determinado produto em cada região (permite identificar as melhores regiões para se comprar determinado produto)
+CREATE MATERIALIZED VIEW mv_preco_medio_regiao AS
 SELECT 
     dl.regiao,
     AVG(fc.preco) AS preco_medio
@@ -30,9 +32,10 @@ ORDER BY preco_medio;
 
 -- Consulta 03: identificação do produtos que são produzidos dentro de um determinado local (pode ser realizada pela região ou pelo estado)
 -- Pela região
+CREATE MATERIALIZED VIEW mv_produtos_regiao AS
 SELECT DISTINCT 
     dp.id_produto,
-	dp.nome_produto,
+dp.nome_produto,
     dp.grupo,
     dp.subgrupo,
     dp.unidade_medida
@@ -43,9 +46,10 @@ WHERE dl.regiao = '<nome_regiao>' -- Alterar para a região desejada no filtro
 ORDER BY dp.grupo, dp.subgrupo;
 
 -- Pelo estado
+CREATE MATERIALIZED VIEW mv_produtos_estado AS
 SELECT DISTINCT 
     dp.id_produto,
-	dp.nome_produto,
+dp.nome_produto,
     dp.grupo,
     dp.subgrupo,
     dp.unidade_medida
@@ -56,6 +60,7 @@ WHERE dl.estado = '<nome_estado>' -- Alterar para o estado desejado no filtro
 ORDER BY dp.grupo, dp.subgrupo;
 
 -- Consulta 04: variações dos preços de acordo com o tipo de produto
+CREATE MATERIALIZED VIEW mv_preco_tipo AS
 SELECT 
     dp.grupo,
     dp.subgrupo,
